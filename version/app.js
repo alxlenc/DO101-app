b615a41aadd4dfee7f4c645c72d26ac699a8852d
Alex Serna <alex.serna@redhat.com>
2021-11-08 15:28:41 +0100

    change version, webhook test

--- version/app.js
+++ version/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello, World VERSION!\n');
+  res.send('Hello, World VERSION 3!\n');
 });
 
 app.listen(8080, function () {
